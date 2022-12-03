3c522b0be41b185d8ea2db23b6d0307f8343294d
secretshardul <aeershardul@gmail.com>
2022-12-03 21:01:56 +0530

    feat: Update stored log from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
-import {ethers} from 'ethers'
+import {ethers, BigNumber as EthersBigNumber} from 'ethers'
+import BigNumber from "bignumber.js"
 import { useWallet } from 'use-wallet'
-import reactLogo from './assets/react.svg'
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree"
 import './App.css'
 import { abi } from './contracts/LogStorage.sol/LogStorage.json'
+import * as treeJson from './tree.json'
 
 function App() {
   const wallet = useWallet()
@@ -11,11 +13,11 @@ function App() {
     ? new ethers.providers.Web3Provider(wallet.ethereum)
     : null
 
-  const [characteristic, setCharacteristic] = useState(0)
-  const [mantissa, setMantissa] = useState(100)
+  const [storedCharacteristic, setStoredCharacteristic] = useState(0)
+  const [storedMantissa, setStoredMantissa] = useState(100)
 
   const [newX, setNewX] = useState<number>(1)
-  const [log, setLog] = useState(0)
+  const [storedLog, setStoredLog] = useState('0')
 
   useEffect(() => {
     if (!provider) {
@@ -24,14 +26,63 @@ function App() {
     const logStorage = new ethers.Contract('0xbF3892B7A68e939bF1ca0DB5f91d5Cc73AaF779A', abi, provider);
 
     logStorage.storedCharacteristic()
-      .then((value: number) => setCharacteristic(value))
+      .then((value: number) => setStoredCharacteristic(value))
 
     logStorage.storedMantissa()
-      .then((value: number) => setMantissa(value))
+      .then((value: number) => setStoredMantissa(value))
+
+      logStorage.storedLogX64()
+      .then((value: EthersBigNumber) => {
+        const x64 = new BigNumber(2).pow(64)
+        const readableLog = new BigNumber(value.toString()).div(x64)
+        setStoredLog(readableLog.toFixed(4, BigNumber.ROUND_DOWN))
+      })
 
   }, [provider])
 
-  const x = Math.pow(10, characteristic) * mantissa / 100
+
+  async function updateLog() {
+    if (!provider) {
+      return
+    }
+    const logStorage = new ethers.Contract('0xbF3892B7A68e939bF1ca0DB5f91d5Cc73AaF779A', abi, provider.getSigner())
+
+    // Generate characteristic and mantissa
+    const char = Math.floor(Math.log10(newX))
+
+    let mantissa = newX
+    if (newX < 10) {
+      mantissa = newX * 100
+    } else if (newX < 100) {
+      mantissa = newX * 10
+    } else if (newX > 999) {
+      mantissa = Math.floor(newX / Math.pow(10, char - 2))
+    }
+
+    // Generate proof
+    // @ts-ignore
+    const tree = StandardMerkleTree.load(treeJson)
+
+    let proof: string[] | undefined
+    let logX64: string | undefined
+
+    for (const [i, v] of tree.entries()) {
+      if (v[0] === mantissa.toString()) {
+        proof = tree.getProof(i);
+        logX64 = v[1]
+      }
+    }
+
+    if (!proof || !logX64) {
+      throw Error('Not found')
+    }
+    const result = await logStorage.updateLog(char, mantissa, logX64, proof, {
+      gasLimit: 1_000_000
+    })
+    console.log('result', result)
+  }
+
+  const x = Math.pow(10, storedCharacteristic) * storedMantissa / 100
 
   return (
     <div className="App">
@@ -41,18 +92,18 @@ function App() {
         </a>
       </div>
       <h1>Napier's Tree</h1>
-      <h2>x = {x}, log (x) = {log}</h2>
+      <h2>x = {x}, log (x) = {storedLog}</h2>
       <div className="card">
         <div>
           <input type="number"
             style={{ marginRight: 10 }}
             value={newX}
-            onChange={event => setNewX(Number(event.target.value))}
+            onChange={event => setNewX(Math.floor(Number(event.target.value)))}
           />
 
           {
             wallet.isConnected()
-              ? <button onClick={() => { }}>
+              ? <button onClick={updateLog}>
                   Calculate log
                 </button>
               : <button onClick={() => wallet.connect()}>Connect MetaMask</button>
